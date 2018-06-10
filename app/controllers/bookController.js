c252d78efe54d8627d7122eed8a0d9dedbd86324
kashishbehl0310 <kashishbehl36@gmail.com>
2018-06-10 18:41:10 +0530

    Database added

--- app/controllers/bookController.js
+++ app/controllers/bookController.js
@@ -1,3 +1,23 @@
+var mysql = require('mysql');
+var config = require('../../config/database_config');
+
+var DB_Config = {
+    database: config.mysql.database,
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password
+};
+
+var connection = mysql.createConnection(DB_Config);
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting to db ", err);
+    }
+    else{
+        console.log("connected");
+    }
+})
+
 const books = [
     {
         title: "3 Mistakes of My Life",
@@ -15,20 +35,36 @@ const books = [
         genre: "Romance"
     }
 ];
+
 exports.bookController = function(req, res){
     // res.send("book");
-   
     var data = {
         title: "Library",
         nav: [
             {title: 'Books', link: '/books'},
             {title: 'Author', link: '/author'}
-        ],
-        books: books
+        ]
     };
-    res.render('booklist', {
-        data: data
-    })
+    (async function query(){
+        var query = "SELECT * FROM books";
+       await connection.query(query, function(err, rows){
+            if(err){
+                console.log(err);
+            }
+            else{
+                data.books = rows;
+                console.log(rows);
+                console.log("results are " + data.books);
+                res.render('booklist', {
+                    data: data
+                })
+            }
+        });
+    }())
+ 
+   
+   
+   
 };
 exports.SingleBookController = function(req, res){
     var id = req.params.id;
--- app/controllers/homePageController.js
+++ app/controllers/homePageController.js
@@ -1,3 +1,25 @@
+// var mysql = require('mssql');
+var mysql = require('mysql');
+var config = require('../../config/database_config');
+
+var DB_Config = {
+    database: config.mysql.database,
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password
+};
+
+var connection = mysql.createConnection(DB_Config);
+connection.connect(function(err){
+    if(err){
+        console.log("Error connecting to db ", err);
+    }
+    else{
+        console.log("connected");
+    }
+})
+// mysql.connect(DB_Config).catch(err => console.log(err));
+
 exports.homePageController = function(req, res){
     var data = {
         title: "Library",
